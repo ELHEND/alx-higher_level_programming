faeabc99b8685c71a1d72c112d4e7a646dce2447
ELHEND <alhnd683@gmail.com>
2023-06-05 10:52:01 -0700

    z

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 += (" " + str2)
-print(Welcome to Holberton School!)
+str1 = "Welcome to "
+str2 = "Holberton School!"
+print(str1 + str2)
