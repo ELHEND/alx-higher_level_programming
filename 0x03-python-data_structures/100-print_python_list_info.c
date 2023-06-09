0632972bfc84ab97f9f062c30660936560bb7f87
ELHEND <alhnd683@gmail.com>
2023-06-09 01:40:53 -0700

    nbhgy

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,14 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+void print_python_list_info(PyObject *p)
+{
+	long int size = PyList_Size(p);
+	int m;
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (m = 0; m < size; m++)
+		printf("Element %i: %s\n", m, Py_TYPE(obj->ob_item[m])->tp_name);
+}
