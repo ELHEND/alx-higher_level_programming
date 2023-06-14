45d91356dda6b2f97fe7d12a2b6a5258bfcd0339
ELHEND <alhnd683@gmail.com>
2023-06-14 10:20:17 -0700

    ews

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """
+    wordA function that computes the square
+    value of all integers of a matrix.
+    """
     new_matrix = []
     for row in matrix:
         new_row = []
