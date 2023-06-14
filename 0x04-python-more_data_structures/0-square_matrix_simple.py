a8675cd1fbb3f373cc5c13493a2578b4a256a538
ELHEND <alhnd683@gmail.com>
2023-06-14 10:26:57 -0700

    wg

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -7,10 +7,7 @@ def square_matrix_simple(matrix=[]):
     value of all integers of a matrix.
     """
     new_matrix = []
-    for row in matrix:
-        new_row = []
-        for num in row:
-            new_row.append(num ** 2)
-        new_matrix.append(new_row)
+    for col in matrix:
+        result = list(map(lambda x: x**2, col))
+        new_matrix.append(result)
     return new_matrix
-
