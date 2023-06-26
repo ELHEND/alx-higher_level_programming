656a314bd1bc90499522dea739c5742fe3679a92
ELHEND <alhnd683@gmail.com>
2023-06-26 13:01:08 -0700

    qhgde

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,17 +6,17 @@ def safe_print_list(my_list=[], x=0):
         x (int): The number of elements of my_list to print.
     Returns:
         The number of elements printed.
-       
+
     """
     ret = 0
     for m in range(x):
         try:
             print("{}".format(my_list[m]), end="")
             ret += 1
-           
+
         except IndexError:
             break
-           
+
     print("")
-   
+
     return (ret)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,13 +11,13 @@ def safe_print_list_integers(my_list=[], x=0):
     for m in range(0, x):
         try:
             print("{:d}".format(my_list[m]), end="")
-           
+
             ret += 1
-                      
+
         except (ValueError, TypeError):
-       
+
             continue
-           
+
     print("")
-   
+
     return (ret)
