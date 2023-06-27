051486f241d650ff34b2e15f05727d5507200e02
ELHEND <alhnd683@gmail.com>
2023-06-27 03:12:47 -0700

    ds

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -10,18 +10,16 @@ class Square:
         """Initialize a new square.
 
         Args:
-
             size (int): The size of the new square.
         """
-
         self.size = size
 
     @property
-
     def size(self):
 
         """Get/set the current size of the square."""
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -29,9 +27,11 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
+
     def my_print(self):
         """Print the square with the # character."""
         for m in range(0, self.__size):
