f1bcc7b775a165cfd0b7a4041d0661f141c77433
ELHEND <alhnd683@gmail.com>
2023-07-19 11:19:34 -0700

    fv

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 """
 test for class base
 """
-
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
@@ -14,14 +12,15 @@ class TestBase(unittest.TestCase):
     """
     class for testing
     """
-
     def test_for_id(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
+
     def test_for_id_twice(self):
         b1 = Base()
         b2 = Base()
         self.assertEqual(b1.id, b2.id - 1)
+
     def test_for_id_third(self):
         b1 = Base()
         b2 = Base()
@@ -36,20 +35,22 @@ class TestBase(unittest.TestCase):
         b1 = Base(None)
         b2 = Base(None)
         self.assertEqual(b1.id, b2 - 1)
+
     def test_for_id_mix(self):
         b1 = Base()
         b2 = Base(12)
         b3 = Base()
         self.assertEqual(b1.id, b3.id - 1)
+
     def test_for_change_value(self):
         b = Base(12)
         b.id = 15
         self.assertEqual(15, b.id)
 
-
     def test_base_with_two_arg(self):
         with self.assertRaises(TypeError):
             Base(1, 1)
+
+
     if __name__ == "__main__":
         unittest.main()
-
