10c6ef0aa1ea662362a2ecc37957f6be1e93aa71
ELHEND <alhnd683@gmail.com>
2023-08-16 10:58:56 -0700

    sf

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
-uninstall plugin validate_password;
+--uninstall plugin validate_password;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '';
 UNINSTALL COMPONENT "file://component_validate_password";
