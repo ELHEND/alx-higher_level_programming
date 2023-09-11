1cc0ffa1b8df3b064d05c2582a0a06dbf2112dc8
ELHEND <alhnd683@gmail.com>
2023-09-11 03:04:26 -0700

    huhu

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
+  let m = 0;
+  while (m < x) {
     console.log('C is fun');
-    i++;
+    m++;
   }
 }
