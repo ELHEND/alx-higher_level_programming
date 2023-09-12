d14878cec7703e74c81016632148a8ffdbd4ea29
ELHEND <alhnd683@gmail.com>
2023-09-12 02:34:36 -0700

    siiiiiii

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -8,7 +8,7 @@ class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) {
+    for (let m = 0; m < this.height; m++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
         s += 'X';
