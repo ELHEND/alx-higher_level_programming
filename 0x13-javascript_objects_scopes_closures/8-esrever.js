61995c15ed2435b666af9b62bf73c04fad1c001b
ELHEND <alhnd683@gmail.com>
2023-09-12 02:28:28 -0700

    x

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   let len = list.length - 1;
-  let i = 0;
-  while ((len - i) > 0) {
+  let m = 0;
+  while ((len - m) > 0) {
     const aux = list[len];
-    list[len] = list[i];
-    list[i] = aux;
-    i++;
+    list[len] = list[m];
+    list[m] = aux;
+    m++;
     len--;
   }
   return list;
