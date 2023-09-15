4c07bc23081b87c8a2fe47a1a454284b3aace6a3
ELHEND <alhnd683@gmail.com>
2023-09-15 02:08:54 -0700

    er

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+""" prints State object with name passed as argument from the database
 """
 import sys
+
 from relationship_state import Base, State
+
 from relationship_city import City
+
 from sqlalchemy import (create_engine)
+
 from sqlalchemy.orm import sessionmaker
+
 from sqlalchemy.orm import relationship
 
 
