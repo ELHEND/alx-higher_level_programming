6e086dfa19fc508b809d87c616e2cd480cf3f6c6
ELHEND <alhnd683@gmail.com>
2023-09-15 01:59:09 -0700

    qaw

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+""" prints  State object with  name passed as argument from the database
 """
+
 import sys
+
 from model_state import Base, State
+
 from sqlalchemy import (create_engine)
+
 from sqlalchemy.orm import sessionmaker
 
 
