acb011c7f2f806a366ae673871df34d978ff01c4
ELHEND <alhnd683@gmail.com>
2023-09-15 02:03:07 -0700

    mnm

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a City
+Contains  class definition of a City
 """
+
 from relationship_state import Base
+
 from sqlalchemy import Column, Integer, String, ForeignKey
+
 from sqlalchemy.ext.declarative import declarative_base
 
 
