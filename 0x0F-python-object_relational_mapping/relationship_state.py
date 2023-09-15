6006e541fd5387ccfd2363efb879e412a41e2305
ELHEND <alhnd683@gmail.com>
2023-09-15 02:04:22 -0700

    ddr

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
-Contains State class and Base, an instance of declarative_base()
+Contains State class and Base,  instance of declarative_base()
 """
+
 from sqlalchemy import Column, Integer, String, MetaData
+
 from sqlalchemy.ext.declarative import declarative_base
+
 from sqlalchemy.orm import relationship
 
 mymetadata = MetaData()
