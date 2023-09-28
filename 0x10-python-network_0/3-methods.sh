4d35378a21f46454ed525803fd0eff2f56c4ffac
ELHEND <alhnd683@gmail.com>
2023-09-28 03:32:27 -0700

    dy

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display all HTTP methods  server of a given URL will accept.
+# Display all HTTP methods  server of  given URL will accept.
 curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
