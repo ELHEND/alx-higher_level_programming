673c4445b408f5f0c4ba3cf7ac6393e2ac6cc1c9
ELHEND <alhnd683@gmail.com>
2023-09-29 07:41:43 -0700

    ff

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A script that:
-- takes in URL,
-- sends request to the URL and displays the value
+- takes in a URL,
+- sends a request to the URL and displays the value
 - of the X-Request-Id variable found in the header ofthe response.
 """
 import sys
